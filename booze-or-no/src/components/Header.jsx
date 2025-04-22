8ae63db1454ce3e5aaa230949c4db988f54d3d27
thisispaniz <paniz.ziaie@gmail.com>
2025-04-22 20:52:11 +0200

    header component

---
+++ booze-or-no/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../App.css";
+
+function Header() {
+    return (
+        <header className="header">
+            <div className="logo">
+                <img src="/Booze or No.svg" alt="BoozeOrNo" />
+            </div>
+            <nav className="nav">
+                <a href="#about" className="nav-link">About</a>
+                <a href="#signup" className="nav-link">Sign up</a>
+                <a href="#login" className="nav-link-yellow">Login</a>
+            </nav>
+        </header>
+    )}
+
+export default Header;
