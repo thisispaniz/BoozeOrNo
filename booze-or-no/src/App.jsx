98e3a9b4ef11a2c26842c62d26d3a1cf2c61cd2b
thisispaniz <paniz.ziaie@gmail.com>
2025-04-24 11:26:04 +0200

    features section done

--- booze-or-no/src/App.jsx
+++ booze-or-no/src/App.jsx
@@ -1,10 +1,12 @@
 import NavBar from './components/NavBar';
 import HeroSection from './components/HeroSection';
+import FeaturesSection from './components/FeaturesSection';
 
 function App() {
   return <div>
           <NavBar />
           <HeroSection />
+          <FeaturesSection />
         </div>
 }
 
