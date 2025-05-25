f9ce54fca956680f89cba12a0c9f1eb2a31e9222
Franzi <115472701+xginevra@users.noreply.github.com>
2025-05-25 07:56:30 +0000

    try to connect with backend

--- frontend/booze-or-no/vite.config.js
+++ frontend/booze-or-no/vite.config.js
@@ -6,6 +6,13 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000,
-    strictPort: true // ensures Vite fails if port 3000 is not available
+    strictPort: true, // ensures Vite fails if port 3000 is not available
+    proxy: {
+      '/search': {
+        target: 'http://backend:8000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   }
 })
