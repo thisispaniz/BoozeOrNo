f59a644eeff533f6de9e9ac6cc93b947a1790c24
Franzi <115472701+xginevra@users.noreply.github.com>
2025-05-25 08:29:14 +0000

    fetching data works now :DDD

--- frontend/booze-or-no/vite.config.js
+++ frontend/booze-or-no/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000,
+    https: false,  // explicitly disable https if it is enabled
     strictPort: true, // ensures Vite fails if port 3000 is not available
     proxy: {
       '/search': {
