702e5fe3a089cbfa796e57e189f79745b5ec7aee
thisispaniz <paniz.ziaie@gmail.com>
2025-05-25 10:48:41 +0200

    routing fixed

--- frontend/booze-or-no/src/App.jsx
+++ frontend/booze-or-no/src/App.jsx
@@ -1,15 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import HeroSection from './components/HeroSection';
 import FeaturesSection from './components/FeaturesSection';
 import DisclaimerSection from './components/DisclaimerSection';
 
+function LandingPage() {
+  return (
+   <>
+    <NavBar />
+    <HeroSection />
+    <FeaturesSection />
+    <DisclaimerSection />
+   </>
+  );
+}
+
 function App() {
-  return <div>
-          <NavBar />
-          <HeroSection />
-          <FeaturesSection />
-          <DisclaimerSection />
-        </div>
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
