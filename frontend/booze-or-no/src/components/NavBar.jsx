abffd0f2e6aaa79cf81886a4cb9a8d7904bf4a14
Paniz <99075803+thisispaniz@users.noreply.github.com>
2025-05-25 12:48:03 +0200

    Update NavBar.jsx

--- frontend/booze-or-no/src/components/NavBar.jsx
+++ frontend/booze-or-no/src/components/NavBar.jsx
@@ -10,7 +10,6 @@ function NavBar() {
             </div>
             <nav className="nav">
                 <a href="#about" className="nav-link">About</a>
-                <link to="/signup" className="nav-link">Sign up</link>
                 <a href="#login" className="nav-link-yellow">Login</a>
             </nav>
         </header>
