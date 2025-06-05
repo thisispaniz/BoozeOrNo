e782ee474fa366947a42060c71a60424ec436902
Franzi <115472701+xginevra@users.noreply.github.com>
2025-06-05 11:53:35 +0200

    Update LoginPage.jsx to fit to the render page

--- frontend/booze-or-no/src/pages/LoginPage.jsx
+++ frontend/booze-or-no/src/pages/LoginPage.jsx
@@ -1,9 +1,23 @@
+// loginpage.jsx
 import React, { useState } from "react";
+import { supabase } from "../supabaseClient";
 import '../App.css';
 import NavBar from "../components/NavBar";
 import TagLine from "../components/TagLine";
 
-const SignupForm = () => {
+const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState(null);
+
+  const handleLogin = async () => {
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    setMessage(error ? error.message : "Login successful!");
+  };
 
   return (
     <>
@@ -11,13 +25,16 @@ const SignupForm = () => {
       <div className="container">
         <h1 className="title">LOGIN TO YOUR ACCOUNT</h1>
         <div className="form">
-          <input className="signup-input" type="email" placeholder="email" />
+          <input className="signup-input" type="email" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
           <input
             className="signup-input"
             type="password"
             placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
-          <button className="register-btn">LOGIN</button>
+          <button className="register-btn" onClick={handleLogin}>LOGIN</button>
+          {message && <p>{message}</p>}
         </div>
       </div>
       <TagLine />
@@ -25,4 +42,4 @@ const SignupForm = () => {
   );
 };
 
-export default SignupForm;
+export default LoginForm;
