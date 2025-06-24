a25ed5ab1103c9d6140a4a34e2a04c9fff1839e0
thisispaniz <paniz.ziaie@gmail.com>
2025-06-24 19:06:06 +0200

    interaction checker

--- frontend/booze-or-no/src/pages/InteractionChecker.jsx
+++ frontend/booze-or-no/src/pages/InteractionChecker.jsx
@@ -16,6 +16,26 @@ function InteractionChecker () {
                     <button className="search-button">BOOZE OR NO</button>
                 </div>
             </div>
+            <div className="medicine-search-results my-5 p-4">
+                <div className=" d-flex flex-column gap-3">
+                <div className="medicine-title-info d-flex flex-column gap-0 p-3 bg-main-dark">
+                    <h2>Darvocet-N</h2>
+                    <p className="m-0">Active Ingredient: <span className="active-ingredient-name fw-bold">Propoxyphene</span></p>
+                </div>
+                <div className="alcohol-interaction p-3 bg-main-dark border-high-risk py-4">
+                    <p className="m-0">Interaction with alcohol: <span className="interaction-risk text-uppercase fw-bold high-risk">high risk</span></p>
+                </div>
+                <div className="side-effects p-3 bg-main-dark">
+                    <p>Alcohol interaction side-effects include:</p>
+                    <ul className="fw-bold fs-5">
+                    <li>Increased risk of overdose</li>
+                    <li>Severe drowsiness</li>
+                    <li>Respiratory depression</li>
+                    <li>Increased risk of liver damage</li>
+                    </ul>
+                </div>
+                </div>
+            </div>
             <Footer />
         </div>
     )
