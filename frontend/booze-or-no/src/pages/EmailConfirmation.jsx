56f29ca3c1d3ae78c54f3a65e88d5663376671b4
Franzi <115472701+xginevra@users.noreply.github.com>
2025-06-25 16:13:37 +0200

    Update EmailConfirmation.jsx

--- frontend/booze-or-no/src/pages/EmailConfirmation.jsx
+++ frontend/booze-or-no/src/pages/EmailConfirmation.jsx
@@ -1,12 +1,10 @@
 import '../App.css';
-import NavBar from "../components/NavBar";
 import Footer from '../components/Footer';
 
 const EmailConfirmation = () => {
 
     return (
         <>
-            <NavBar />
             <h1>Thank you for confirming your email!</h1>
             <button className="register-btn">LOGIN</button>
             <Footer />
