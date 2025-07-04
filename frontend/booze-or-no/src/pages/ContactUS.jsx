3c0f8672d20b7d593412c2b921ac642084c2333f
Franzi <115472701+xginevra@users.noreply.github.com>
2025-07-04 21:13:15 +0200

    Create ContactUS.jsx

---
+++ frontend/booze-or-no/src/pages/ContactUS.jsx
@@ -0,0 +1,13 @@
+import Footer from "../components/Footer";
+
+const ContactUs = () =>  {
+    return (
+        <div className="page-container">
+            <h1>CONTACT</h1>
+            <p>If you have any questions, issues, or requests, please feel free to email us at info@boozeorno.com.</p>
+            <Footer />
+        </div>
+    );
+};
+
+export default ContactUs;
