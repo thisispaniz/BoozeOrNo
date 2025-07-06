986cf702bffbd172a2b0390650190d3a7f7afcef
Paniz <99075803+thisispaniz@users.noreply.github.com>
2025-07-06 09:34:31 +0200

    fixed login link

--- frontend/booze-or-no/src/pages/EmailConfirmation.jsx
+++ frontend/booze-or-no/src/pages/EmailConfirmation.jsx
@@ -1,12 +1,13 @@
 import '../App.css';
 import Footer from '../components/Footer';
+import { Link } from "react-router-dom"; <Link to="/login" className="nav-link-yellow">Login</Link>
 
 const EmailConfirmation = () => {
 
     return (
         <>
             <h1>Thank you for confirming your email!</h1>
-            <button className="register-btn">LOGIN</button>
+            <Link to="/login" className="nav-link-yellow">Login</Link>
             <Footer />
         </>
     );
